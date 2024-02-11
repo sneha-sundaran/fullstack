0bb215c3866af81ebe21498277c438f14f743c56
sneha sundaran mp <snehapallippuram@gmail.com>
2024-02-12 04:23:36 +0530

    add redux persist

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return <BrowserRouter>
   <Header/> 
   <Routes>
-    < Route path="/" element={<Home />} /> 
+    < Route path="/" element={<Home/>} /> 
     < Route path="/sign-in" element={<Signin />} /> 
     < Route path="/sign-up" element={<SignUp />} /> 
     < Route path="/About" element={<About />}   /> 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <PersisGate loading={null} persistor={persistor}>
     <App />
-  </React.StrictMode>,
-)
+    </PersisGate>
+  </Provider>
+);
+   
